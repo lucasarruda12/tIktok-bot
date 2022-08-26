81990b63b8d75bd7ece34db18731f6d2d7b5b3e1
Lucas Manoel <89367719+lucasarruda12@users.noreply.github.com>
2022-08-26 18:54:15 -0300

    Text-to-speak e screenshot do post - Finalizados

--- RedditAPI/index.py
+++ RedditAPI/index.py
@@ -1,7 +1,9 @@
 import praw
 from credentials import *
-import os
+import pyttsx3
+from playwright.sync_api import sync_playwright
 
+# GET REDDIT SUBMISSION
 reddit = praw.Reddit(
     client_id = client_id, 
     client_secret = client_secret, 
@@ -12,10 +14,29 @@ reddit = praw.Reddit(
 
 top = reddit.subreddit("desabafos").hot(limit = 12)
 
-id = -2
-path = "temp"
+# SET UP TEXT-TO-SPEAK
+engine = pyttsx3.init()
+
+rate = engine.getProperty('rate')                
+engine.setProperty('rate', 250) 
+
+# SET UP POST SCREENSHOT
+def capture(url: str, id:int) -> None:
+    page = p.chromium.launch().new_page()
+    page.set_viewport_size({"width": 450, "height": 660})
+    page.goto(url)
+    posts = page.locator("data-testid=post-container")
+    posts.first.screenshot(path=str(id)+".png")
+
+# TEXT-TO-SPEAK FUNCTION -> SAVE TO FILE
+id= -2
+path= "temp"
 for submission in top:
-    f = open(os.path.join(path, str(id)+".txt"), "w+", encoding="utf-8")
-    f.write(submission.title + '\n\n\n' + submission.selftext + '\n\n\n')
-    f.close()
-    id = id+1
+    texto = str(submission.title + submission.selftext)
+    if(len(texto) < 1115):
+        engine.save_to_file(texto, str(id)+".mp3")
+        engine.runAndWait()
+        with sync_playwright() as p:
+            capture(submission.url, id)
+            p.chromium.launch().close()
+            id = id+1
